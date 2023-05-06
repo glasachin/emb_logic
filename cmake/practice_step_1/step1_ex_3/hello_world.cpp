d38e6587e2161593b614ea0b898dfae9b38e3dd9
sachin <glasachin@yahoo.co.in>
2023-05-06 13:43:38 +0530

    update

---
+++ cmake/practice_step_1/step1_ex_3/hello_world.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include "config.h"
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello world from CPP"<<endl;
+    cout << Tutorial_VERSION_MAJOR << endl;
+    return 0;
+}
