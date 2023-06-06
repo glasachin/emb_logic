b3156d466c54df6d906de5180810e5676311c868
sachin <glasachin@yahoo.co.in>
2023-06-06 10:26:55 +0530

    update

--- 04_Network_programming/notes_examples/07_7_eg_getsocket.c
+++ 04_Network_programming/notes_examples/07_7_eg_getsocket.c
@@ -1,4 +1,4 @@
-/*bind is used to give name to the socket*/
+/*This file implements the getsockaddr function to get the socket address*/
 #include<stdio.h>
 #include<unistd.h>
 #include<stdlib.h>
@@ -10,42 +10,56 @@
 #include<netinet/in.h>
 #include<arpa/inet.h>
 
-/*to report errors*/
-static void bail(const char *on_what){
+static void bail(const char *on_what)
+{
     perror(on_what);
     exit(1);
 }
 
-int main(int argc, char **argv, char **envp){
+char * sock_addr(int s, char *buf, size_t bufsiz)
+{
+    int z;
+    struct sockaddr_in adr_inet; 
+    int len_inet;
 
+    len_inet = sizeof adr_inet;
+    z = getsockname(s, (struct sockaddr *) &adr_inet, &len_inet);
+
+    if(z == -1)
+        return NULL;
+    
+    snprintf(buf,bufsiz,"%s:%u",inet_ntoa(adr_inet.sin_addr),(unsigned)ntohs(adr_inet.sin_port));
+
+    return buf;
+}
+
+int main(int argc, char **argv, char **envp)
+{
     int z;
     int sck_inet;
     struct sockaddr_in adr_inet;
     int len_inet;
+    char buf[64];
 
-    /*create an IPv4 Internet Socket*/
     sck_inet = socket(AF_INET, SOCK_STREAM, 0);
-
     if(sck_inet == -1)
         bail("socket()");
     
-    /*Create and AF_INET address*/
     memset(&adr_inet, 0, sizeof adr_inet);
     adr_inet.sin_family = AF_INET;
     adr_inet.sin_port = htons(9000);
-
     inet_aton("127.0.0.24", &adr_inet.sin_addr);
 
-    len_inet = sizeof adr_inet;
-
-    /*bind address to the socket*/
+    /*bind the address*/
     z = bind(sck_inet, (struct sockaddr *)&adr_inet, len_inet);
     if(z == -1)
         bail("bind()");
     
-    // system("netstat -pa --tcp 2 > /dev/null | sed -n '1,/^Proto/p;/bind/p'");
+    if(!sock_addr(sck_inet, buf, sizeof buf))
+        bail("sock_addr()");
+    
+    printf("Address is '%s'\n", buf);
 
     close(sck_inet);
-
     return 0;
 }
