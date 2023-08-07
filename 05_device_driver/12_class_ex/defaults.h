2bbea1558282f8286eed71e875f52c4b2ff961bf
Sachin Sharma <glasachin@yahoo.co.in>
2023-08-07 22:35:55 +0530

    new

---
+++ 05_device_driver/12_class_ex/defaults.h
@@ -0,0 +1,19 @@
+#ifndef DEBUG
+#define DEBUG
+#endif
+
+#ifndef CDDNAME
+#define CDDNAME "MyCharDev"
+#endif
+
+#ifndef MAJORNO
+#define MAJORNO 0
+#endif
+
+#ifndef MINORNO
+#define MINORNO 0
+#endif
+
+#ifndef NOD
+#define NOD 20
+#endif
---
+++ 05_device_driver/12_class_ex/operations.h
@@ -0,0 +1,5 @@
+struct file_operations fops=
+{
+    open:openDevice,
+    release : releaseDevice
+};
