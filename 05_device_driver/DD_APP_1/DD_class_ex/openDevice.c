beba5cca8cf36b79b71401f0fca9e83d5304850b
Sachin Sharma <glasachin@yahoo.co.in>
2023-08-18 22:52:05 +0530

    update

--- 05_device_driver/DD_APP_1/DD_class_ex/openDevice.c
+++ 05_device_driver/DD_APP_1/DD_class_ex/openDevice.c
@@ -5,14 +5,27 @@
 
 int openDevice(struct inode *inode, struct file *file)
 {
+    Dev *ldev;
+
     #ifdef DEBUG
     printk(KERN_INFO "Begin: %s\n", __func__);
     #endif
     
     
+    ldev = container_of(inode->i_cdev, Dev, c_dev);
+    if(!ldev)
+    {
+        printk(KERN_ERR "%s: Error container_of() failure.\n", __func__);
+        goto OUT;
+    }
 
     #ifdef DEBUG
-    printk(KERN_INFO "End: %s\n", __func__);
+        printk(KERN_INFO "End: %s\n", __func__);
     #endif
     return 0;
+    OUT:
+    #ifdef DEBUB
+        printk(KERN_ERR "%s: Error: End\n", __func__);
+    #endif
+    return -1;
 }
