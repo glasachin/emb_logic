daf15c651f2cdd2db2ca0c8d8239232ebd1b0a12
Sachin Sharma <glasachin@yahoo.co.in>
2023-08-25 20:04:33 +0530

    update

--- 05_device_driver/DD_APP_1/DD_class_ex/openDevice.c
+++ 05_device_driver/DD_APP_1/DD_class_ex/openDevice.c
@@ -19,6 +19,20 @@ int openDevice(struct inode *inode, struct file *file)
         goto OUT;
     }
 
+    /*
+    filep->private_data = ldev;
+    if((filep->f_flags & O_ACCMODE) == O_WRONLY)
+        filep->f_pos = 0;
+    else if((filep->f_flags & O_ACCMODE) == O_RDONLY)
+        printk(KERN_INFO "%s: Device opened for Reading\n", __func__);
+    else if((filep->f_flags & O_ACCMODE) == O_APPEND)
+        printk(KERN_INFO "%s: Device Opened for Appending\n", __func__);
+    else
+    {
+        printk(KERN_INFO "%s: Invalid Device Open Mode \n", __func__);
+        goto OUT:
+    }
+    */
     #ifdef DEBUG
         printk(KERN_INFO "End: %s\n", __func__);
     #endif
