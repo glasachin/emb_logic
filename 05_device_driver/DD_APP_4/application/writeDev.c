6a0172950637fde3addbeebce8e58bb6d388d690
Sachin Sharma <glasachin@yahoo.co.in>
2023-09-27 22:39:37 +0530

    update

--- 05_device_driver/DD_APP_4/application/writeDev.c
+++ 05_device_driver/DD_APP_4/application/writeDev.c
@@ -7,12 +7,20 @@ void* writeDev(void *arg)
 {
     char ch;
     char ch1[15] = "Hello World!";
+    char ch2[9] = "Kernel!";
+    int n;
     printf("%s: Begin \n", __func__);
     
-    // Do write() system call
-    // ch = 'K';
-    // write(fd, &ch, sizeof(ch));
-    write(fd, ch1, sizeof(ch1));
+    // we are not holding the thread so direct reading will not work
+    printf("Which string want to write 1 or 2?\n");
+    scanf("%d", &n);
+    
+    if( n == 1)
+        write(fd, ch1, sizeof(ch1));
+    else if(n == 2)
+        write(fd, ch2, strlen(ch2)); // don't use strlen for empty string
+    else
+        write(fd, ch1, sizeof(ch1));
 
     printf("%s: End\n", __func__);
     return 0;
